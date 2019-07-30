82c750678a41d2cf25eccdcf6ecfa9117b04161f
Kiran Krishnan <typetokrish@gmail.com>
2019-07-30 17:17:27 +0530

    basics of jascript

---
+++ basics/basics.js
@@ -0,0 +1,51 @@
+
+console.log("hello");
+
+var a=100;
+console.log(a);
+console.log(typeof a);
+var str="hai";
+console.log(typeof str);
+console.log(x);
+
+var x=10;
+
+var c =100;
+
+function demo()
+{
+    var k = 200;
+    console.log(k);
+    c = 120;
+    console.log(c);
+}
+
+demo();
+//console.log(k);
+console.log(c);
+var d=100;
+if(d=='100')
+{
+    console.log("equal");
+}
+
+var assign;
+
+if(!assign){
+    console.log('not set')
+}
+var cd ='err';
+
+
+var m = function() {
+    console.log('hello');
+    
+}
+
+var m2 = (a) => {  a*a}
+
+m();
+m2(100);
+
+
+console.log(isNaN(cd));
